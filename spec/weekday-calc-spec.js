ee7cc84b04178fa722fb8db4a69e5b952466dda6
Katlin Anderson and megan schulte <megan@>
2019-05-29 14:59:54 -0700

    add passing test

--- spec/weekday-calc-spec.js
+++ spec/weekday-calc-spec.js
@@ -1,8 +1,12 @@
-import { weekdayCalc } from './weekday-calc';
+
+import { weekdayCalc } from './../src/weekday-calc.js';
 
 describe('weekdayCalc', function() {
 
-  it('it should #####', function() {
-    //Test content will go here.
+  it('it should return name of day of week based on weekdayValue ', function() {
+    let date = new Date(2019, 4, 12);
+    console.log(date);
+    console.log(date.getDay());
+    expect(weekdayCalc(date)).toEqual("Sunday");
   });
 });
