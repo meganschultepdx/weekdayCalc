58fecd01b8a84046d3b4211b39b8d5c587ad8a3a
Katlin Anderson and megan schulte <megan@>
2019-05-29 15:43:00 -0700

    add styling to ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { weekdayCalc } from './weekday-calc';
-import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
 $(document).ready(function() {
   $('#date-form').submit(function(event) {
