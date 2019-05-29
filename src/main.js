49292a8e6351b7659096cac966aa2f6535d66887
Katlin Anderson and megan schulte <megan@>
2019-05-29 10:23:41 -0700

    add initial files and webpack

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import {} from './weekday-calc';
+import './styles.css';
+
+$(document).ready(function() {
+  $('#date-form').submit(function(event) {
+    event.preventDefault();
+    //var goal = $('#goal').val();
+    //var output = pingPong(goal);
+    //output.forEach(function(element) {
+      $('#solution').append("stuffhere");
+    // });
+  });
+});
